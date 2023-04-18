3fb4231bba9809eb3491a8423f65b0f79b14155b
Davide Mucci <davidemucci0804@gmail.com>
2023-04-18 10:38:08 +0200

    Add DbContext

---
+++ Core/MyPlaceBusinessLogic/Contexts/MyPlaceDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyPlace.BusinessLogic.Entities;
+using MyPlace.BusinessLogic.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlace.BusinessLogic.Contexts
+{
+    public class MyPlaceDbContext : DbContext 
+    {
+        public MyPlaceDbContext(DbContextOptions<MyPlaceDbContext> options) : base(options) { }
+
+
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+
+            modelBuilder.Entity<Reservation>(builder =>
+            {
+                builder.Property(x => x.Date)
+                    .HasConversion<Helpers.DateOnlyConverter, DateOnlyComparer>();
+
+            });
+        }
+
+    }
+}
